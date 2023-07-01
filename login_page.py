722909220d953043719513ef44494e84cfa0ed8f
Toaf86 <tonytoaf@gmail.com>
2023-07-01 00:12:06 -0600

    login page conex

--- login_page.py
+++ login_page.py
@@ -1,10 +1,14 @@
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-opsiones = int(input("seleccion una Opsion \n1.Inicio de secion \n2.Cerrar secion.\nNumero de eleccion: "))
-print (opsiones)
+# Desactivar las advertencias de verificación de certificados SSL
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-if opsiones == 1:  #para inciar la secion 
-    login_url = 'https://conex.rnp.hn'
+opciones = int(input("Seleccione una opción: \n1. Inicio de sesión \n2. Cerrar sesión \nNúmero de elección: "))
+print(opciones)
+
+if opciones == 1:  # para iniciar la sesión
+    login_url = 'https://conex.rnp.hn/LoginUsuario.aspx'
     username = input("Usuario: ")
     password = input("Contraseña: ")
 
@@ -13,23 +17,22 @@ if opsiones == 1:  #para inciar la secion
         'password': password
     }
 
-    response = requests.post(login_url, data=payload)
+    response = requests.post(login_url, data=payload, verify=False)
 
     if response.status_code == 200:
         print("Inicio de sesión exitoso")
     else:
         print("Error en el inicio de sesión")
 
-elif opsiones == 2: #para cerrar la secion 
-    logout_url = 'https://conex.rnp.hn'
+elif opciones == 2:  # para cerrar la sesión
+    logout_url = 'https://conex.rnp.hn/CerrarSesion.aspx'
 
-    response = requests.get(logout_url)
+    response = requests.get(logout_url, verify=False)
 
     if response.status_code == 200:
         print("Sesión cerrada exitosamente")
     else:
         print("Error al cerrar la sesión")
 
-
 else:
-    print("ninguna opsion seleccionada")
+    print("Ninguna opción seleccionada")
