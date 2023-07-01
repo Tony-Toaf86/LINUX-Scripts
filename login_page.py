82b27acd832b8481206f2522e2d2c1b0804c10c1
Toaf86 <tonytoaf@gmail.com>
2023-06-30 19:59:01 -0600

    login page conex

---
+++ login_page.py
@@ -0,0 +1,35 @@
+import requests
+
+opsiones = int(input("seleccion una Opsion \n1.Inicio de secion \n2.Cerrar secion.\nNumero de eleccion: "))
+print (opsiones)
+
+if opsiones == 1:  #para inciar la secion 
+    login_url = 'https://conex.rnp.hn/LoginUsuario.aspx'
+    username = input("Usuario: ")
+    password = input("Contraseña: ")
+
+    payload = {
+        'username': username,
+        'password': password
+    }
+
+    response = requests.post(login_url, data=payload)
+
+    if response.status_code == 200:
+        print("Inicio de sesión exitoso")
+    else:
+        print("Error en el inicio de sesión")
+
+elif opsiones == 2: #para cerrar la secion 
+    logout_url = 'https://conex.rnp.hn/LoginUsuario.aspx'
+
+    response = requests.get(logout_url)
+
+    if response.status_code == 200:
+        print("Sesión cerrada exitosamente")
+    else:
+        print("Error al cerrar la sesión")
+
+
+else:
+    print("ninguna opsion seleccionada")
